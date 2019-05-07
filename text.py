9aa166405ef3c5b603c60f1c4bb4d784ac634b18
olagood <renton@openmailbox.org>
2019-05-07 04:48:09 +0300

    Added command to translate ASCII letter characters to unicode negative squared letter characters

--- text.py
+++ text.py
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 class Module:
     def __init__(self):
         self.commands = ['ae', 'circled_text', 'negative_circled_text',
-                         'squared_text']
+                         'squared_text', 'negative_squared_text']
         self.helpmsg = [
             "Usage: .ae <Text>",
             "       .circled_text <Text>",
@@ -62,6 +62,15 @@ _SQUARED_ALP_U_MAP = dict((i, (i - 0x41) + 0x1F130) for i in range(0x41, 0x5B))
 _SQUARED_ALP_L_MAP = dict((i, (i - 0x61) + 0x1F130) for i in range(0x61, 0x7B))
 SQUARED_MAP = {**_SQUARED_ALP_U_MAP, **_SQUARED_ALP_L_MAP}
 
+_NEGATIVE_SQUARED_ALP_U_MAP = dict(
+    (i, (i - 0x41) + 0x1F170) for i in range(0x41, 0x5B))
+_NEGATIVE_SQUARED_ALP_L_MAP = dict(
+    (i, (i - 0x61) + 0x1F170) for i in range(0x61, 0x7B))
+NEGATIVE_SQUARED_MAP = {
+    **_NEGATIVE_SQUARED_ALP_U_MAP,
+    **_NEGATIVE_SQUARED_ALP_L_MAP
+}
+
 
 def main(i, irc):
     if not i.msg_nocmd:
@@ -78,5 +87,7 @@ def main(i, irc):
         t = s.translate(NEGATIVE_CIRCLED_MAP)
     elif i.cmd == "squared_text":
         t = s.translate(SQUARED_MAP)
+    elif i.cmd == "negative_squared_text":
+        t = s.translate(NEGATIVE_SQUARED_MAP)
 
     irc.privmsg(i.channel, t)
