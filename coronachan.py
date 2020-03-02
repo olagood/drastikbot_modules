c9ca7748007ba9d182ab3f11c09437d6ec9cb4f6
olagood <derezzed@protonmail.com>
2020-03-02 20:41:07 +0200

    bugfix

--- coronachan.py
+++ coronachan.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.worldometers.info/coronavirus/'
 
+
 class Module:
     def __init__(self):
         self.commands = ['corona', 'coronachan']
@@ -11,21 +12,23 @@ class Module:
             " ",
             "Useful stats related to 2019-nCov"]
 
+
 def main(i, irc):
     page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    cases = extract_from_page('Coronavirus Cases:', soup)
-    deaths = extract_from_page('Deaths:', soup)
-    recovered = extract_from_page('Total Cured:', soup)
+    soup = BeautifulSoup(page.text, "html.parser")
+    cases = extract_from_page("Coronavirus Cases:", soup)
+    deaths = extract_from_page("Deaths:", soup)
+    recovered = extract_from_page("Recovered:", soup)
 
     irc.privmsg(
         i.channel,
-        (f"\x02Total cases:\x0F {cases} | "
-         f"\x02Dead:\x0F \x0304{deaths}\x0F | " 
-         f"\x02Recovered:\x0F \x0303{recovered}\x0F | "
-         f"\x02Source:\x0F {url}")
+        (f"\x02Total cases:\x0F {cases}"
+         f" | \x02Dead:\x0F \x0304{deaths}\x0F"
+         f" | \x02Recovered:\x0F \x0303{recovered}\x0F"
+         f" | \x02Source:\x0F {url}")
     )
 
+
 def extract_from_page(text, soup):
-    return soup.body.find(text=text).parent.parent.find("div", { "class": "maincounter-number" }).find("span").text
+    return soup.find(text=text).parent.parent.find(
+        "div", {"class": "maincounter-number"}).find("span").text
