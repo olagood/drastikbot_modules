2dff2bd0805775afec6e9714e70526d7ed17a828
olagood <derezzed@protonmail.com>
2020-07-01 02:39:40 +0300

    fixed youtube and twitter title fetching by using oembed

--- url.py
+++ url.py
@@ -7,7 +7,7 @@
 #   - beautifulsoup :: $ pip3 install beautifulsoup4
 
 '''
-Copyright (C) 2017-2019 drastik.org
+Copyright (C) 2017-2020 drastik.org
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
@@ -142,30 +142,16 @@ def default_parser(u):
 #                                            #
 def youtube(url):
     '''Visit a video and get it's information.'''
-    try:
-        r = requests.get(url,
-                         headers={"user-agent": user_agent,
-                                  "Accept-Language": accept_lang},
-                         timeout=10)
-        soup = bs4.BeautifulSoup(r.text, parser)
-        name = soup.find(attrs={"itemprop": "name"})['content']
-        channel = soup.find(attrs={"class": "yt-user-info"}).text.strip()
-        duration = soup.find(attrs={"itemprop": "duration"})['content']
-        duration = duration[2:][:-1].split('M')
-        mins = int(duration[0])
-        if mins > 59:
-            mins = '{:02d}:{:02d}'.format(*divmod(mins, 60))  # wont do days
-        if len(duration[1]) < 2:
-            secs = f'0{duration[1]}'
-        else:
-            secs = duration[1]
-            duration = f'{mins}:{secs}'
-            out = (f"\x0300,04 ► \x0F: {name} ({duration})"
-                   f" | \x02Channel:\x0F {channel}")
-        return out
-    except Exception:
+    logo = "\x0300,04 ► \x0F"
+    u = f"https://www.youtube.com/oembed?url={url}"
+    r = requests.get(u, timeout=10)
+    if r:
+        j = r.json()
+        return (f"{logo}: {j['title']}"
+                f" | \x02Channel:\x0F {j['author_name']}")
+    else:
         out = default_parser(url)[0]
-        return f"\x0300,04 ► \x0F: {out}"
+        return f"{logo}: {out}"
 
 
 def lainchan(url):
@@ -259,6 +245,23 @@ def nitter(url):
         return output
     except Exception:
         return output
+
+
+def twitter(url):
+    logo = "\x0311twitter\x0F"
+    u = f"https://publish.twitter.com/oembed?url={url}"
+    r = requests.get(u, timeout=10,
+                     headers={"user-agent": user_agent,
+                              "Accept-Language": accept_lang})
+    if r:
+        j = r.json()
+        html = j["html"]
+        soup = bs4.BeautifulSoup(html, parser)
+        tweet = soup.get_text(separator=" ")
+        return f"{logo}: {tweet}"
+    else:
+        out = default_parser(url)[0]
+        return f"{logo}: {out}"
 #                                          #
 # END: Website Handling Functions (by url) #
 #                                          #
@@ -270,7 +273,8 @@ hosts_d = {
     "lainchan.org": lainchan,
     "i.imgur.com": imgur,
     "imgur.com": imgur,
-    "nitter.net": nitter
+    "nitter.net": nitter,
+    "twitter.com": twitter
 }
 
 
