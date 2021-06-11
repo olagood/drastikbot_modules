934731a77d936c8cc3ca9eac8c5be10d4b9cd1db
drastik <derezzed@protonmail.com>
2021-06-11 21:57:39 +0300

    Switched to the new module API.

--- tell.py
+++ tell.py
@@ -8,7 +8,7 @@
 # .tell drastik drastikbot is down | .tell <NICKNAME> <MESSAGE>
 
 '''
-Copyright (C) 2017 drastik.org
+Copyright (C) 2017, 2021 drastik.org
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,24 +25,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import datetime
+
+from dbothelper import is_ascii_cl
 from ignore import is_ignored
 
 
 class Module:
-    def __init__(self):
-        self.commands = ['tell']
-        self.auto = True
-        self.manual = {
-            "desc": (
-                "Send a message to a user through the bot. This is used to"
-                " send messages to users that are AFK or not connected to "
-                "the server. The bot will message the receiver as soon as "
-                "they post to a channel that the bot has joined."
-            ),
-            "bot_commands": {
-                "tell": {"usage": lambda x: f"{x}tell <receiver> <message>"}
-            }
+    irc_commands = ["PRIVMSG"]
+    manual = {
+        "desc": (
+            "Send a message to a user through the bot. This is used to"
+            " send messages to users that are AFK or not connected to "
+            "the server. The bot will message the receiver as soon as "
+            "they post to a channel that the bot has joined."
+        ),
+        "bot_commands": {
+            "tell": {"usage": lambda x: f"{x}tell <receiver> <message>"}
         }
+    }
 
 
 def add(receiver, msg, sender, dbc):
@@ -61,8 +61,8 @@ def find(nick, irc, dbc):
                     'WHERE receiver=?;', (nick,))
         fetch = dbc.fetchall()
         for i in fetch:
-            irc.privmsg(nick, f'\x0302{i[2]} [{i[0]} UTC]:\x0F')
-            irc.privmsg(nick, i[1])
+            irc.out.notice(nick, f'\x0302{i[2]} [{i[0]} UTC]:\x0F')
+            irc.out.notice(nick, i[1])
         dbc.execute('''DELETE FROM tell WHERE receiver=?;''', (nick,))
         # Delete messages older than 3600 * 24 * 30 * 3 seconds.
         dbc.execute('''DELETE FROM tell WHERE
@@ -73,26 +73,44 @@ def find(nick, irc, dbc):
 
 
 def main(i, irc):
-    dbc = i.db[1].cursor()
-    if 'tell' == i.cmd:
-        try:
-            arg_list = i.msg_nocmd.split(' ', 1)
-            receiver = arg_list[0]
-            msg = arg_list[1]
-        except IndexError:
-            help_msg = f"Usage: {i.cmd_prefix}{i.cmd} <Receiver> <Message>"
-            return irc.privmsg(i.channel, help_msg)
-        if i.nickname.lower() == receiver.lower():
-            return irc.privmsg(i.channel, 'You can tell yourself that.')
-        if irc.var.curr_nickname.lower() == receiver.lower():
-            return irc.privmsg(i.channel,
-                               f'{i.nickname}: I am here now, tell me.')
-        if is_ignored(i, irc, receiver, i.nickname):
-            return  # say nothing
-        add(receiver, msg, i.nickname, dbc)
-        irc.privmsg(i.channel,
-                    f'{i.nickname}: I will tell {receiver} '
-                    'when they are around.')
+    db = i.db_disk
+    dbc = db.cursor()
 
-    find(i.nickname, irc, dbc)
-    i.db[1].commit()
+    msgtarget = i.msg.get_msgtarget()
+    nickname = i.msg.get_nickname()
+
+    botcmd = i.msg.get_botcmd()
+    prefix = i.msg.get_botcmd_prefix()
+    args = i.msg.get_args()
+
+    if "tell" == botcmd:
+        argv = args.split(" ", 1)
+
+        try:
+            receiver = argv[0]
+            message = argv[1]
+        except IndexError:
+            m = f"Usage: {prefix}{botcmd} <receiver> <message>"
+            irc.out.notice(msgtarget, m)
+            return
+
+        if is_ascii_cl(irc.curr_nickname, receiver):
+            m = f"{nickname}: I am here now, tell me."
+            irc.out.notice(msgtarget, m)
+            return
+
+        if is_ignored(i, irc, receiver, nickname):
+            return  # say nothing
+
+        add(receiver, message, nickname, dbc)
+
+        if i.msg.is_nickname(receiver):
+            m = f'{nickname}: When you return, I will notify'
+            irc.out.notice(msgtarget, m)
+            return
+
+        m = f'{nickname}: When {receiver} returns, I will notify.'
+        irc.out.notice(msgtarget, m)
+
+    find(nickname, irc, dbc)
+    db.commit()
