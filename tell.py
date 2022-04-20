44d88ce28633d4549ad342ff1fc5318c5aef312b
drastik <derezzed@protonmail.com>
2022-04-20 19:55:57 +0300

    Added a feature so that you know when a tell message was sent from a channel or from a pm with the bot. Compartmentized the code so that the db will stop producing errors.

--- tell.py
+++ tell.py
@@ -31,6 +31,7 @@ from ignore import is_ignored  # type: ignore
 
 
 class Module:
+    startup = True
     irc_commands = ["PRIVMSG"]
     manual = {
         "desc": (
@@ -45,73 +46,152 @@ class Module:
     }
 
 
-def add(receiver, msg, sender, dbc):
-    timestamp = datetime.datetime.utcnow().replace(microsecond=0)
-    dbc.execute("CREATE TABLE IF NOT EXISTS tell "
-                "(receiver TEXT COLLATE NOCASE, msg TEXT, sender TEXT, "
-                "timestamp TEXT, date INTEGER);")
-    dbc.execute("INSERT INTO tell VALUES (?, ?, ?, ?, strftime('%s', 'now'));",
-                # These are wrong but handled correctly. Fix it someday.
-                (receiver, msg, str(timestamp), sender))
+# ====================================================================
+# Insert messages in the db
+# ====================================================================
 
-
-def find(nick, irc, dbc):
-    try:
-        dbc.execute('SELECT sender, msg, timestamp FROM tell '
-                    'WHERE receiver=?;', (nick,))
-        fetch = dbc.fetchall()
-        for i in fetch:
-            irc.out.privmsg(nick, f'\x0302{i[2]} [{i[0]} UTC]:\x0F')
-            irc.out.privmsg(nick, i[1])
-        dbc.execute('''DELETE FROM tell WHERE receiver=?;''', (nick,))
-        # Delete messages older than 3600 * 24 * 30 * 3 seconds.
-        dbc.execute('''DELETE FROM tell WHERE
-                    (strftime('%s', 'now') - date) > (3600 * 24 * 30 * 3);''')
-    except Exception:
-        # Nothing in the db yet, ignore errors.
-        pass
-
-
-def main(i, irc):
-    db = i.db_disk
-    dbc = db.cursor()
-
+def add(i, irc):
     msgtarget = i.msg.get_msgtarget()
-    ch_pfx = i.bot["conf"].get_channel_prefix(msgtarget)
     nickname = i.msg.get_nickname()
 
     botcmd = i.msg.get_botcmd()
     prefix = i.msg.get_botcmd_prefix()
+
     args = i.msg.get_args()
+    argv = args.split(" ", 1)
 
-    if "tell" == botcmd and i.msg.is_botcmd_prefix(ch_pfx):
-        argv = args.split(" ", 1)
+    try:
+        receiver = argv[0]
+        message = argv[1]
+    except IndexError:
+        m = f"Usage: {prefix}{botcmd} <receiver> <message>"
+        irc.out.notice(msgtarget, m)
+        return
 
-        try:
-            receiver = argv[0]
-            message = argv[1]
-        except IndexError:
-            m = f"Usage: {prefix}{botcmd} <receiver> <message>"
-            irc.out.notice(msgtarget, m)
-            return
+    if is_ascii_cl(irc.curr_nickname, receiver):
+        m = f"{nickname}: I am here now, tell me."
+        irc.out.notice(msgtarget, m)
+        return
 
-        if is_ascii_cl(irc.curr_nickname, receiver):
-            m = f"{nickname}: I am here now, tell me."
-            irc.out.notice(msgtarget, m)
-            return
+    if is_ignored(i, irc, receiver, nickname):
+        return  # do not say anything
 
-        if is_ignored(i, irc, receiver, nickname):
-            return  # say nothing
+    db_insert(i, receiver, message, nickname, msgtarget)
 
-        add(receiver, message, nickname, dbc)
-
-        if i.msg.is_nickname(receiver):
-            m = f'{nickname}: When you return, I will notify'
-            irc.out.notice(msgtarget, m)
-            return
-
+    if i.msg.is_nickname(receiver):
+        m = f'{nickname}: When you return, I will notify'
+        irc.out.notice(msgtarget, m)
+    else:
         m = f'{nickname}: When {receiver} returns, I will notify.'
         irc.out.notice(msgtarget, m)
 
-    find(nickname, irc, dbc)
+
+def db_insert(i, receiver, msg, sender, msgtarget):
+    db = i.db_disk
+    dbc = db.cursor()
+
+    timestamp = datetime.datetime.utcnow().replace(microsecond=0)
+    sql = """
+        INSERT INTO tell VALUES (?, ?, ?, ?, strftime('%s', 'now'), ?);
+    """
+    dbc.execute(sql, (receiver, msg, sender, str(timestamp), msgtarget))
+
     db.commit()
+
+
+# ====================================================================
+# Pull messages from the db
+# ====================================================================
+
+def find(i, irc):
+    db = i.db_disk
+    dbc = db.cursor()
+    nick = i.msg.get_nickname()
+
+    sql = """
+        SELECT sender, msg, timestamp, channel FROM tell WHERE receiver=?;
+    """
+    dbc.execute(sql, (nick,))
+
+    fetch = dbc.fetchall()
+    if not fetch:
+        return
+
+    for x in fetch:
+        header, msg = prep_message(x)
+        irc.out.privmsg(nick, header)
+        irc.out.privmsg(nick, msg)
+
+    dbc.execute("DELETE FROM tell WHERE receiver=?;", (nick,))
+    # Delete messages older than 3600 * 24 * 30 * 3 seconds.
+    sql = """
+        DELETE FROM tell
+        WHERE (strftime('%s', 'now') - date) > (3600 * 24 * 30 * 3);
+    """
+    dbc.execute(sql)
+
+    db.commit()
+
+
+def prep_message(fetchdata):
+    sender, msg, timestamp, channel = fetchdata
+
+    header = f'\x02\x0312{sender}\x0F \x0315[{timestamp} UTC]\x0F'
+    if sender == channel:
+        header += " \x0304[Private]\x0F:"
+    else:
+        header += f" \x0303[{channel}]\x0F:"
+    return header, msg
+
+
+# ====================================================================
+# Intialization / Migrations
+# ====================================================================
+
+def db_init(i):
+    db = i.db_disk
+    dbc = db.cursor()
+
+    dbc.executescript("""
+    CREATE TABLE IF NOT EXISTS tell (
+        receiver  TEXT COLLATE NOCASE,
+        msg       TEXT,
+        sender    TEXT,
+        timestamp TEXT,
+        date      INTEGER,
+        channel   TEXT);
+    """)
+
+    # Migration code :: added 2022/20/4 :: delete after 2023/20/4
+    dbc.execute("PRAGMA table_info(tell);")
+    if not any([x[1] == "channel" for x in dbc.fetchall()]):
+        dbc.execute("ALTER TABLE tell ADD COLUMN channel TEXT;")
+
+    db.commit()
+
+
+# ====================================================================
+# Main
+# ====================================================================
+
+def main(i, irc):
+    try:
+        botcmd = i.msg.get_botcmd()
+    except AttributeError:
+        botcmd = i.msg.get_command()  # __STARTUP
+
+    if "__STARTUP" == botcmd:
+        db_init(i)
+        return
+
+    msgtarget = i.msg.get_msgtarget()
+    ch_pfx = i.bot["conf"].get_channel_prefix(msgtarget)
+
+    find(i, irc)
+
+    if i.msg.is_botcmd_prefix(ch_pfx):
+        if "tell-initialize" == botcmd:
+            db_init(i)
+
+        if "tell" == botcmd:
+            add(i, irc)
